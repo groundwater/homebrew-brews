558191da9805d6d78712c3d24ea1001ef9ba30aa
Jacob Groundwater <groundwater@gmail.com>
2013-04-28 11:09:10 -0700

    Point to New Project

--- mix.rb
+++ mix.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mix < Formula
 
-  url 'https://gist.github.com/jacobgroundwater/fcf267c3449e64997542/download'
-  homepage 'https://gist.github.com/jacobgroundwater/fcf267c3449e64997542'
-  sha1 '86acb0caa377c5e2504fb2375c8039e2970ca2b3'
+  url 'https://github.com/jacobgroundwater/Mix.git'
+  homepage 'https://github.com/jacobgroundwater/Mix'  
+  version "0.0.1"
   
 end
