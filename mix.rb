c722b1276c030b3b708d43928c8a1aafba74d558
Jacob Groundwater <groundwater@gmail.com>
2013-04-28 11:10:54 -0700

    Add Install Definition

--- mix.rb
+++ mix.rb
@@ -6,4 +6,9 @@ class Mix < Formula
   homepage 'https://github.com/jacobgroundwater/Mix'  
   version "0.0.1"
   
+  def install
+    bin.install 'bin/mix'
+    prefix.install Dir['lib']
+  end
+  
 end
