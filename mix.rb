ca0d480e07823409e88e5acec2789e34b8d1d9b3
Jacob Groundwater <groundwater@gmail.com>
2013-04-28 08:41:19 -0700

    Add Example Formula

---
+++ mix.rb
@@ -0,0 +1,9 @@
+require 'formula'
+
+class Mix < Formula
+
+  url 'https://gist.github.com/jacobgroundwater/fcf267c3449e64997542/download'
+  homepage 'https://gist.github.com/jacobgroundwater/fcf267c3449e64997542'
+  sha1 '86acb0caa377c5e2504fb2375c8039e2970ca2b3'
+  
+end
